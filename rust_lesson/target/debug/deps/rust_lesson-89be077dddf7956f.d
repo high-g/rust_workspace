c3fdaa1cc862920bcc4e870901aa25ec4ff09517
high-g <altema1217bbfumi@live.jp>
2024-05-17 21:19:57 +0900

    Initial commit for Rust lesson

---
+++ rust_lesson/target/debug/deps/rust_lesson-89be077dddf7956f.d
@@ -0,0 +1,5 @@
+/Users/tani.fumiya/workspace/rust_lesson/target/debug/deps/librust_lesson-89be077dddf7956f.rmeta: src/main.rs
+
+/Users/tani.fumiya/workspace/rust_lesson/target/debug/deps/rust_lesson-89be077dddf7956f.d: src/main.rs
+
+src/main.rs:
