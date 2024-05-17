8a9920bd9e67b8622fd6a70cc18258a8211423e9
high-g <altema1217bbfumi@live.jp>
2024-05-17 21:27:35 +0900

    Refactor main.rs file and update dependencies

--- rust_lesson/src/main.rs
+++ rust_lesson/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    /*print!("Hello");
+    print!("Rust");*/
+    println!("Hello, {}", "student");
+
+    let a: i32 = 1;
+    println!("{}", a);
+
+    // a = 2;
+
+    let mut b: i32 = 2;
+    b = 3;
+
+    println!("{}", b);
 }
