c870c70ecbb3822336c22fb5b62813af3971ebc6
high-g <altema1217bbfumi@live.jp>
2024-05-18 15:22:37 +0900

    Vector

--- rust_lesson/src/main.rs
+++ rust_lesson/src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let t1: (i32, bool, f32) = (2, true, 3.0);
-
-    println!("{} {} {}", t1.0, t1.1, t1.2);
-    println!("{:?}", t1);
-
-    let (x, y, z) = t1;
-
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<i32> = vec![0; 1000];
     
+    let mut v3: Vec<i32> = Vec::new();
+    v3.push(1);
+    v3.push(2);
+    v3.push(3);
+    println!("{:?}", v3);
+
+    let x: Option<i32> = v3.pop();
+    println!("{:?}", x);
+    println!("{:?}", v3[1]);
+
+
+    let z: Option<&i32> = v3.get(1);
+    println!("{:?}", z);
 }
