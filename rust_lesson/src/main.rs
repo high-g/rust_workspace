ddaa173e844b9d942637f884f31e3f5ef5e3ca6c
high-g <altema1217bbfumi@live.jp>
2024-05-20 22:49:41 +0900

    refactor: Optimize loop logic and update dependencies in main.rs

--- rust_lesson/src/main.rs
+++ rust_lesson/src/main.rs
@@ -1,10 +1,17 @@
+use std::rc::Rc;
+
 fn main() {
+    let x: Box<i32> = Box::new(1);
+    println!("x: {:p}", x);
+    println!("*x +2 = {}", *x + 2);
+
+    let a: Rc<String> = Rc::new("hello".to_string());
+    println!("count1: {}", Rc::strong_count(&a));
     {
-        let r: &i32;
-        {
-            let x: i32 = 1;
-            r = &x;
-            println!("{}", r);
-        }
+        let b: Rc<String> = Rc::clone(&a);
+        println!("a: {:p}", a);
+        println!("b: {:p}", b);
+        println!("count2: {}", Rc::strong_count(&a));
     }
+    println!("count3: {}", Rc::strong_count(&a));
 }
