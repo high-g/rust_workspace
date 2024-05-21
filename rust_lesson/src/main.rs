7b19c3c5eec27ecd5f4a11a90de55f85050a025a
high-g <altema1217bbfumi@live.jp>
2024-05-21 23:59:12 +0900

    refactor: Optimize loop logic and update dependencies in main.rs

--- rust_lesson/src/main.rs
+++ rust_lesson/src/main.rs
@@ -1,6 +1,30 @@
-use rand::Rng;
+mod test_module {
+    pub mod sub_module1 {
+        pub fn test_fn1() {
+            println!("test_fn1_1");
+        }
+
+        fn test_fn2() {
+            println!("test_fn1_2");
+        }
+    }
+
+    mod sub_module2 {
+        pub fn test_fn1() {
+            println!("test_fn2_1");
+        }
+
+        fn test_fn2() {
+            println!("test_fn2_2");
+        }
+    }
+}
+
+use test_module::sub_module1;
 
 fn main() {
-    let random_number: i32 = rand::thread_rng().gen_range(1..101);
-    println!("{}", random_number);
+    // crate::test_module::sub_module1::test_fn1();
+    // test_module::sub_module1::test_fn1();
+
+    sub_module1::test_fn1();
 }
