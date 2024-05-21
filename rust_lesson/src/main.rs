caafc084cbc4aeb5a6846e05982bca5600c031f6
high-g <altema1217bbfumi@live.jp>
2024-05-21 23:26:21 +0900

    refactor: Optimize loop logic and update dependencies in main.rs

--- rust_lesson/src/main.rs
+++ rust_lesson/src/main.rs
@@ -1,23 +1,5 @@
-use std::vec;
-
+use rand::Rng;
 fn main() {
-    // enum Option<T> {
-    //     None,
-    //     Some(T),
-    // }
-
-    let a: Option<i32> = Some(5);
-    let b: Option<&str> = Some("Hello");
-    let c: Option<i32> = None;
-
-    let v: Vec<i32> = vec![1, 2, 3, 4, 5];
-    let val: Option<&i32> = v.get(0);
-
-    match val {
-        // Some(1) => println!("Value is: 1"),
-        // Some(2 | 3) => println!("Value is: 2 or 3"),
-        Some(x) if *x == 1 => println!("Value is: 1"),
-        Some(x) => println!("Value is: {}", x),
-        None => println!("Value not found"),
-    }
+    let random_number = rand::thread_rng().gen_range(1..101);
+    println!("{}", random_number);
 }
