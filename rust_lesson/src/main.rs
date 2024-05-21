52f8d956d019b7936e6e27ff6a9e3fe99d8d3861
high-g <altema1217bbfumi@live.jp>
2024-05-21 23:35:22 +0900

    refactor: Optimize loop logic and update dependencies in main.rs

--- rust_lesson/src/main.rs
+++ rust_lesson/src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
+
 fn main() {
-    let random_number = rand::thread_rng().gen_range(1..101);
+    let random_number: i32 = rand::thread_rng().gen_range(1..101);
     println!("{}", random_number);
 }
